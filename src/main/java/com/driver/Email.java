9690374e561f4c1a3dba6f2c688b71eca788ae30
Sumit Joshi <sumitmumbai007@gmail.com>
2022-12-30 20:24:31 +0530

    First commit

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -18,12 +18,54 @@ public class Email {
         return password;
     }
 
-    public void changePassword(String oldPassword, String newPassword){
+    public void changePassword(String oldPassword, String newPassword) {
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+
+        //Start Here
+
+        if (oldPassword.equals(password)) {
+            if (!checkValid(newPassword)) {
+                System.out.println("Invalid Password!!");
+            } else {
+                this.password = newPassword;
+                System.out.println("Password Changed!!");
+            }
+        }
+        else{
+            System.out.println("Does not Match Current Password!!");
+        }
+    }
+    public boolean checkValid(String pass){
+        if(pass.length() < 8){
+            return false;
+        }
+        //flags
+        Boolean caps = false;
+        Boolean small = false;
+        Boolean num = false;
+        Boolean spChar = false;
+
+        for(int i = 0; i<pass.length(); i++){
+            char ch = pass.charAt(i);
+            if((ch >= 'A') && (ch <= 'Z')){
+                caps = true;
+            }
+            else if((ch >= 'a') && (ch <= 'z')){
+                small = true;
+            }
+            else if((ch >= '0') && (ch <= '9')){
+                num = true;
+            }
+            else if(!((ch >= 'A') && (ch <= 'Z')) && !((ch >= 'a') && (ch <= 'z')) && !((ch >= '0') && (ch <= '9'))){
+                spChar = true;
+            }
+        }
+        return caps && small && num && spChar;
     }
 }
+
--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -12,12 +12,13 @@ public class Workspace extends Gmail{
 
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
-
+        super(emailId, Integer.MAX_VALUE);
+        this.calendar = new ArrayList<>();
     }
 
     public void addMeeting(Meeting meeting){
         //add the meeting to calendar
-
+        calendar.add(meeting);
     }
 
     public int findMaxMeetings(){
@@ -25,6 +26,29 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
+        ArrayList<Pair<LocalTime, Integer>> meetingEndTime = new ArrayList<>();
 
+        for(int i = 0; i<calendar.size(); i++){
+            meetingEndTime.add(Pair.of(calendar.get(i).getEndTime(), i));
+        }
+        Collections.sort(meetingEndTime);
+
+        LocalTime time = meetingEndTime.get(0).getLeft();
+
+        int count = 0;
+
+        if(!meetingEndTime.isEmpty()){
+            count++;
+        }
+
+        for(int i = 1; i<meetingEndTime.size(); i++){
+            int compareObj = calendar.get(meetingEndTime.get(i).getRight()).getStartTime().compareTo(time);
+            if( compareObj>0){
+                count++;
+
+                time = meetingEndTime.get(i).getLeft();
+            }
+        }
+        return count;
     }
 }
